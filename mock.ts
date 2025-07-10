91987472d078a35e1c6683f4460b14bb76bcb928
davidgabriellm <davidglm.2002@gmail.com>
2025-07-10 15:37:37 -0300

    feat: mock creation

---
+++ mock.ts
@@ -0,0 +1,54 @@
+interface Product {
+  ID: number;
+  product_name: string;
+  product_price: number;
+  image_url: string;
+  descripition?: string;
+}  
+
+export const bestSeller: Product[] = [
+    {
+        ID: 1,
+        product_name: "bola de futebol nike pinch",
+        product_price: 80,
+        image_url: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTQ7VZsLnHLNxIhCwySj9Mh_slPn6L77zMqQ3s3PnwYRuerfKKdwPzhLFoTaDIoV5SL805yC4tGBZQmwuFEslcE_TWjCaZDwJKcKDWl1tCUkoTtEIdvQxxL9QeFrZtdmy-WOxHuuu2baw&usqp=CAc"
+    },
+    
+    {
+        ID: 2,
+        product_name: "Chuteira Society Original Para Campo Adulto Envio Imediato",
+        product_price: 120,
+        image_url: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSYP9H_PBhQxRpyXuAclDzr3YlNRgsWUWpzryZ9ZD843cpd0ly06huvBRuVpDMY4t9ze5iyM_NFFwBLHVE4NB4bupstKOsiBsNHNuC_Nk0hg9-mVYGfQt8O-k3-PGb2kHXBsi5E9A&usqp=CAc"
+    },
+    {
+        ID: 3,
+        product_name: "Tênis Olympikus Venum",
+        product_price: 130,
+        image_url: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQsRSRCkJgEjTY8lge_d4BtNUCdiqyS-G_2-mFYSmIqwW6DmGB6PBFCRiQtahKbmSeyQOPsBqyfc-s4jVDVVRZlSH-XTduUVmnOxPFlo3KYPwj8fCzwfFQtd2qbfiOMgvsVKG-UoCc&usqp=CAc"
+    },
+    {
+        ID: 4,
+        product_name: "camisa do flamengo 2019",
+        product_price: 100,
+        image_url: "https://acdn-us.mitiendanube.com/stores/003/147/909/products/tmp_b64_173183e7-43d9-418f-a674-a6daeb5f9617_3147909_5604943-cac2a18218a6c3bef717375827586678-1024-1024.webp"
+    },
+    {
+        ID: 5,
+        product_name: "camisa do lakers",
+        product_price: 70,
+        image_url: "https://img4.dhresource.com/webp/m/0x0/f3/albu/ys/l/18/a5c50377-4dd1-48f0-922c-19b42eeb1659.png"
+    },
+    {
+        ID: 6,
+        product_name: "camisa do barcelona 2009",
+        product_price: 95,
+        image_url: "https://acdn-us.mitiendanube.com/stores/003/147/909/products/tmp_b64_0f9e83f3-9753-4bdc-8ee0-4f1b3592c7f5_3147909_5604943-1e944fe5e903dd40eb17375834994935-1024-1024.webp"
+    },
+    {
+        ID: 7,
+        product_name: "chuteira da nike do Neymar 2015",
+        product_price: 200,
+        image_url: "https://venifutebol.com.br/cdn/shop/files/ChuteiraCampoNikeZoomMercurialVapor15EliteFGLaranjaePreta2_b2404b3b-08bf-4207-8258-df0a68e8d483.png?v=1705689833"
+    },
+
+]
